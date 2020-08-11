12d4544153e612c9318f0ebfeef1c721fc267054
Victoria Filileeva <onexsan@gmail.com>
2020-08-11 20:01:16 +0300

    the fraud page: burger menu fixed

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -3,24 +3,24 @@ const menu = document.querySelector('.hover-menu');
 const openBurger = document.querySelector('#menu__toggle');
 
 
-openBtn.addEventListener('mouseenter', function(e) {
+openBtn.addEventListener('mouseenter', function (e) {
   e.preventDefault();
   menu.style["display"] = 'flex';
 
 });
 
-menu.addEventListener('mouseleave', function() {
+menu.addEventListener('mouseleave', function () {
   menu.style['display'] = 'none';
 });
 
 
-openBurger.addEventListener('click', function(e) {
+openBurger.addEventListener('click', function (e) {
   if (e.currentTarget.checked) {
     menu.style["display"] = 'flex';
   } else {
     menu.style["display"] = 'none';
   }
-  
+
 });
 
 
