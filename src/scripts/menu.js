48e15440ceab3de307f213b59faa4cce57349973
Nataliya <n.jelonkina@mail.ru>
2020-08-11 13:50:46 +0300

    burger beg

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,5 +1,6 @@
 const openBtn = document.querySelector('.app-menu__link');
 const menu = document.querySelector('.hover-menu');
+const openBurger = document.querySelector('.menu__button');
 
 
 openBtn.addEventListener('mouseenter', function(e) {
@@ -10,6 +11,14 @@ openBtn.addEventListener('mouseenter', function(e) {
 
 menu.addEventListener('mouseleave', function() {
   menu.style['display'] = 'none';
-  console.log('!!!!');
 });
+
+
+// openBurger.addEventListener('click', function(e) {
+//   e.preventDefault();
+//   menu.style["display"] = 'flex';
+//   console.log('!!!!!');
+// });
+
+
 
