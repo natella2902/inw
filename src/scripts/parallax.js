8f43f9407f7f63f56ebd95f3135fa9a24374e537
Victoria Filileeva <onexsan@gmail.com>
2020-08-16 18:22:07 +0300

    global card page: parallax final

--- src/scripts/parallax.js
+++ src/scripts/parallax.js
@@ -1,7 +1,7 @@
 var image = document.getElementsByClassName('thumbnail');
 new simpleParallax(image, {
 	overflow: true,
-	delay: .6,
+	delay: 1.3,
 	transition: 'cubic-bezier(0,0,0,1)',
-	scale: 1.5
+	scale: 1.9
 });
