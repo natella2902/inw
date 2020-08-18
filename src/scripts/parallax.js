f38513f43bbbd3fa730076300144fe1dd84b26f2
Victoria Filileeva <onexsan@gmail.com>
2020-08-18 16:00:22 +0300

    animations added

--- src/scripts/parallax.js
+++ src/scripts/parallax.js
@@ -1,3 +1,4 @@
+// параллакс для слайдинга монеток на странице global card
 var image = document.getElementsByClassName('thumbnail');
 new simpleParallax(image, {
 	overflow: true,
@@ -6,8 +7,11 @@ new simpleParallax(image, {
 	scale: 1.9
 });
 
-var scene = document.getElementById('scene');
-var parallaxInstance = new Parallax(scene, {
+// параллакс для движения мышью
+
+// global card
+var scene = document.getElementsByClassName('scene');
+new Parallax(scene, {
 	selector: ".layer",
 	frictionX: 0.04,
 	frictionY: 0.04
@@ -15,6 +19,42 @@ var parallaxInstance = new Parallax(scene, {
 
 var paymentScene = document.getElementById('paymentscene');
 var parallaxInstance = new Parallax(paymentScene, {
+	selector: ".layer",
+	frictionX: 0.04,
+	frictionY: 0.04
+});
+
+// payment methods
+
+var payMethods = document.getElementById('paymethodscene');
+var parallaxInstance = new Parallax(payMethods, {
+	selector: ".layer",
+	frictionX: 0.04,
+	frictionY: 0.04
+});
+
+// online payments 
+
+var onlineScene = document.getElementById('onlinescene');
+var parallaxInstance = new Parallax(onlineScene, {
+	selector: ".layer",
+	frictionX: 0.04,
+	frictionY: 0.04
+});
+
+// omni
+
+var omniScene = document.getElementById('omniscene');
+var parallaxInstance = new Parallax(omniScene, {
+	selector: ".layer",
+	frictionX: 0.04,
+	frictionY: 0.04
+});
+
+// checkout
+
+var checkScene = document.getElementById('checkscene');
+var parallaxInstance = new Parallax(checkScene, {
 	selector: ".layer",
 	frictionX: 0.04,
 	frictionY: 0.04
