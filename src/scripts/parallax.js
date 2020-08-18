315e6cfac8b860b1c866d8a34fdf80f2de17e29f
Victoria Filileeva <onexsan@gmail.com>
2020-08-18 15:20:32 +0300

    the paymethods page: parallax and animations added

--- src/scripts/parallax.js
+++ src/scripts/parallax.js
@@ -1,7 +1,6 @@
-var image = document.getElementsByClassName('thumbnail');
-new simpleParallax(image, {
-	overflow: true,
-	delay: 1.7,
-	transition: 'cubic-bezier(0,0,0,1)',
-	scale: 1.9
+var scene = document.getElementById('scene');
+var parallaxInstance = new Parallax(scene, {
+	selector: ".layer",
+	frictionX: 0.04,
+	frictionY: 0.04
 });
