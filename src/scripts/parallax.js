b8d650c4fe0ccbfce79df2f60c420aafc451905b
Victoria Filileeva <onexsan@gmail.com>
2020-08-21 15:56:47 +0300

    extra parallax fixes

--- src/scripts/parallax.js
+++ src/scripts/parallax.js
@@ -1,61 +1,28 @@
 // параллакс для слайдинга монеток на странице global card
-var image = document.getElementsByClassName('thumbnail');
-new simpleParallax(image, {
-	overflow: true,
-	delay: 1.7,
-	transition: 'cubic-bezier(0,0,0,1)',
-	scale: 1.9
-});
+var images = document.getElementsByClassName('thumbnail');
 
-// параллакс для движения мышью
+for (let i = 0; i < images.length; i++) {
+	new simpleParallax(images[i], {
+		overflow: true,
+		delay: 1.7,
+		transition: 'cubic-bezier(0,0,0,1)',
+		scale: 1.9
+	});
+}
 
-// global card
-var scene = document.getElementById('scene');
-var parallaxInstance = new Parallax(scene, {
-	selector: ".layer",
-	frictionX: 0.04,
-	frictionY: 0.04
-});
+// // параллакс для движения мышью
 
-var paymentScene = document.getElementById('paymentscene');
-var parallaxInstance = new Parallax(paymentScene, {
-	selector: ".layer",
-	frictionX: 0.04,
-	frictionY: 0.04
-});
+// // global card
+// var scene = document.getElementById('scene');
+// var parallaxInstance = new Parallax(scene, {
+// 	selector: ".layer",
+// 	frictionX: 0.04,
+// 	frictionY: 0.04
+// });
 
-// payment methods
-
-var payMethods = document.getElementById('paymethodscene');
-var parallaxInstance = new Parallax(payMethods, {
-	selector: ".layer",
-	frictionX: 0.04,
-	frictionY: 0.04
-});
-
-// online payments 
-
-var onlineScene = document.getElementById('onlinescene');
-var parallaxInstance = new Parallax(onlineScene, {
-	selector: ".layer",
-	frictionX: 0.04,
-	frictionY: 0.04
-});
-
-// omni
-
-var omniScene = document.getElementById('omniscene');
-var parallaxInstance = new Parallax(omniScene, {
-	selector: ".layer",
-	frictionX: 0.04,
-	frictionY: 0.04
-});
-
-// checkout
-
-var checkScene = document.getElementById('checkscene');
-var parallaxInstance = new Parallax(checkScene, {
-	selector: ".layer",
-	frictionX: 0.04,
-	frictionY: 0.04
-});
+// var paymentScene = document.getElementById('paymentscene');
+// var parallaxInstance = new Parallax(paymentScene, {
+// 	selector: ".layer",
+// 	frictionX: 0.04,
+// 	frictionY: 0.04
+// });
